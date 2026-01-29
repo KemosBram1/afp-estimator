62f177af1c8c9d58bd0aee55e37788447497744b
Bramuel Kemoli Kidavi <bramuelkemoli@ATPHMAC-KMF251QFRV.local>
2026-01-29 15:38:31 +0300

    Auto-update from SharePoint on 2026-01-29 15:38:31

--- set-git-push.sh
+++ set-git-push.sh
@@ -10,7 +10,9 @@
 # -------------------------------------------------------------------------------
 
 REPO_PATH="/Users/bramuelkemoli/Library/CloudStorage/OneDrive-AssociatedFireProtection/SLM - Sales & Marketing (2025) - src"
-BRANCH="main"# Go to the local GitHub repository directory
+BRANCH="main"
+
+# Go to the local GitHub repository directory
 cd "$REPO_PATH" || exit 1
 
 # Pull the latest changes from the GitHub repository
